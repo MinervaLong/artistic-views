b16e78cb11f8b78b769cafe040f7d672dae11252
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-06-05 10:12:03 +0200

    First version of navBar component

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,17 +1,34 @@
 import React from 'react' 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import Header from '../components/Header'
+import About from '../components/About'
 import HowItWorks from '../components/HowItWorks'
 import Footer from '../components/Footer'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 
 const HomePage = () => {
     return(
-        <div>
-            <NavBar />
-            <Header />
-            <HowItWorks />
-            <Footer />
-        </div>
+        
+            <Router>
+              <NavBar />
+              <Header />
+              <About />
+              <HowItWorks />
+              <Footer />            
+                <div>
+                    <Switch>
+                        <Route exact path='../views/HomePage.js' component={HomePage} />
+                        <Route path='../components/About' component={About} />
+                        <Route path='../components/HowItWorks.js' component={HowItWorks} />
+                        <Route path='./SignIn' component={SignIn} />
+                        <Route path='./SignUp.js' component={SignUp} />
+                    </Switch>
+                </div>
+                
+            </Router>
+        
     )
 }
 export default HomePage;
