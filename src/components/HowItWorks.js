f9099fbd95a60aa71fc4f3feff470efa7d01ed82
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-06-06 17:31:14 +0200

    Add image & diagram to HIW section

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,39 +1,40 @@
 import React from 'react' 
 import { Box, Heading, Tabs, Tab, Button } from 'grommet'
 import DiagramHIW from '../assets/diagramHIW.png'
+import DiagramHIW2 from '../assets/diagramHIW2.png'
+import imageHIW from '../assets/imageHIW.svg'
 
 const HowItWorks = () => {
     return(
         <Box
-            id='#howItWorks'
-            height='medium'
-            align='center'    
-            animation='fadeIn'
-            flex='shrink'
-            pad='small'
-            responsive={true}
+            overflow='hidden'
+            direction='row-responsive'
+            justify='evenly'
+            alignContent='center'
+            style={{alignItems:'center'}}
         >
-            <Heading size='small' style={{fontFamily: 'Spectral'}} >How It Works</Heading>     
-        
             <Box>
-                <Tabs margin='medium'>
+                <img  
+                style={{width:'25rem'}}
+                alt='Loupes looking at computer' 
+                src={imageHIW} />
+
+            </Box> 
+        
+            <Box >
+                <Heading alignSelf='center'  size='small' style={{fontFamily: 'Spectral', textDecoration:'underline'}} >How It Works</Heading>   
+                <Tabs margin='medium' >
                     <Tab title="I'm an artist" margin='none'>
-                        <Box pad="medium" width='large'>
-                            <p>
-                            Etiam non neque vitae massa hendrerit fermentum non quis orci.
-                            Pellentesque maximus est placerat aliquam euismod. Mauris facilisis
-                            malesuada dui a mattis. Morbi id consectetur augue, at dictum sem.
-                            Etim non neque vitae massa hendrerit fermentum non quis orci.
-                            Pellentesque maximus est placerat aliquam euismod. Mauris facilisis
-                            malesuada dui a mattis. Morbi id consectetur augue, at dictum sem.
-                            </p> 
-                            <Button margin='small' alignSelf='center' primary label="Join" />                                                          
+                        <Box pad="medium" width='large'height='medium'>
+                            <img alt='diagram about how it works the app' src={DiagramHIW2} />
+                            
+                            <Button size='large' margin='medium' alignSelf='center' primary label="Join" />                                                          
                         </Box>
                     </Tab>
                     <Tab title="I have a restaurant">
-                        <Box pad="medium" width='large'>
+                        <Box pad="medium" width='large' height='medium'>
                             <img alt='diagram about how it works the app' src={DiagramHIW} />
-                            <Button margin='small' alignSelf='center' primary label="Join" />   
+                            <Button size='large' margin='medium' alignSelf='center' primary label="Join" />   
                         </Box>
                     </Tab>
                 </Tabs>
