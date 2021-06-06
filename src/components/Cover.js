ab0c49b8fbf5345027411455dd86a9626a7122cf
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-06-06 18:30:59 +0200

    Add conditional context to the cover

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,17 +1,33 @@
 import React from 'react' 
 import {Box, Image } from 'grommet'
 import CoverImg from '../assets/cover.png'
+import { ResponsiveContext } from 'grommet'
 
 const Cover = () => {
     return(
-        <Box height="medium" fill='horizontal'  style={{boxShadow: '0px 4px 8px rgba(0,0,0,0.20)'}}>
-            <Image
-                style={{backgroundPosition:'center center'}}         
-                fit="cover"
-                src={CoverImg}
-                a11yTitle='Coverimage with Artistic Views Logo and Eat with Art slogan'
-            />
-        </Box>
+        <ResponsiveContext.Consumer>
+            {size => (  
+                (size === 'large') ? 
+                (<Box height="large" fill='horizontal'  style={{boxShadow: '0px 4px 8px rgba(0,0,0,0.20)'}}>
+                    <Image
+                        style={{backgroundPosition:'center center'}}         
+                        fit="cover"
+                        src={CoverImg}
+                        a11yTitle='Coverimage with Artistic Views Logo and Eat with Art slogan'
+                    />
+                </Box>)
+                : 
+                (<Box height="medium" fill='horizontal'  style={{boxShadow: '0px 4px 8px rgba(0,0,0,0.20)'}}>
+                        <Image
+                            style={{backgroundPosition:'center center'}}         
+                            fit="cover"
+                            src={CoverImg}
+                            a11yTitle='Coverimage with Artistic Views Logo and Eat with Art slogan'
+                        />
+                    </Box>)               
+            
+            )}
+        </ResponsiveContext.Consumer>
     )
 }
 
