ee423a6d50078853732f0349c1f8aa1b5f98b783
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-06-06 12:49:09 +0200

    Fix the Cover width

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -4,8 +4,9 @@ import CoverImg from '../assets/cover.png'
 
 const Cover = () => {
     return(
-        <Box height="medium" width="xlarge" style={{boxShadow: '0px 4px 8px rgba(0,0,0,0.20)'}}>
+        <Box height="medium" fill='horizontal'  style={{boxShadow: '0px 4px 8px rgba(0,0,0,0.20)'}}>
             <Image
+                style={{backgroundPosition:'center center'}}         
                 fit="cover"
                 src={CoverImg}
                 a11yTitle='Coverimage with Artistic Views Logo and Eat with Art slogan'
