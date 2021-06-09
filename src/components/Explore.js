dbaed9dc6c0b3ca663194e090150c5c7b425fdbd
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-06-09 16:02:14 +0200

    Explore component

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,17 +1,50 @@
-import React from 'react' 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import UserCard from './UserCard'
+import {Box, Heading} from 'grommet'
+
+const BASE_URL = 'https://dummyapi.io/data/api/user?limit=12';
+const APP_ID = '60c08cbe9c9883b1fb33da7b';
+const header = {
+    method: 'GET',
+    headers: {
+        'app-id': APP_ID
+    }
+}
 
 const Explore = () => {
-    return(
-            <div>
-                {/*
-                    UserCard components from users 
-                    latest adittions
-                 */}
-                 <UserCard />
-                 <p>Explore component where the users profiles appears</p>
-            </div>
-    )
-}
+    // Call to users API
+    const [data, setData] = useState([]);    
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(async () => {
+        const result = await axios.get(BASE_URL,header)
+        const usersList  = result.data.data
+        console.log('usersList',usersList)
+        setData(usersList)       
+    },[]);
+    
+    
+    const cardComponent = data.map((user)=> {
+        return <UserCard 
+                    key={user.id} 
+                    userImg={user.picture}
+                    userName={user.firstName}
+                    lastName={user.lastName}                     
+                    contact={user.email}
+                />
+    })
+
+    return (
+        <Box 
+            
+        >
+            <Heading level='2' alignSelf='center'>Find your next artist</Heading>
+            <Box direction='row-responsive'justify='evenly' wrap={true}>
+                {cardComponent}
+            </Box>
+        </Box>
+    );
+};
 
 export default Explore;
