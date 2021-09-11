14d5e71d0810e1253c22401ea58d97dac6bf99ff
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-09-11 18:03:45 +0200

    Complete  HIW component

--- src/components/composed/HowItWorks/HowItWorks.js
+++ src/components/composed/HowItWorks/HowItWorks.js
@@ -15,11 +15,19 @@ const HowItWorks = () => {
 
             </div> 
         
-            <div >
+            <div className='howItWorks'>
                 <h2>How It Works</h2> 
-                <p>Here I explain how it works</p>
+                <p>
+                    Bicycle rights irony actually neutra typewriter lyft. 
+                    Man bun taxidermy put a bird on it, umami yr ramps pop-up ugh bushwick 
+                    chia lo-fi. Occupy bitters pour-over snackwave you probably haven't 
+                    heard of them small batch. Cornhole mustache man bun letterpress 
+                    echo park VHS.  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
                 <Link to='/sign-up'>
-                    <button>Join</button> 
+                    <button>Join !</button> 
                 </Link> 
                 
             </div>
--- src/components/composed/HowItWorks/HowItWorks.style.js
+++ src/components/composed/HowItWorks/HowItWorks.style.js
@@ -2,5 +2,54 @@ import styled from "styled-components";
 //import Colors from "theme/Colors";
 
 export const HowItWorksWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+    overflow: hidden;
 
+    .howItWorks {
+        width: 40%;
+        text-align: center;
+    }
+
+    h2 {
+        text-align: center;
+        text-decoration: underline;
+    }
+
+    p{
+        text-align: center;
+        line-height: 3ch;
+    }
+
+    button {
+        min-width: 130px;
+        height: 40px;
+        color: #fff;
+        padding: 5px 10px;
+        font-weight: bold;
+        cursor: pointer;
+        transition: all 0.3s ease;
+        position: relative;
+        display: inline-block;
+        outline: none;
+        border-radius: 20px;
+        border: 2px solid #495057;
+        background: #722D78;
+        
+    }
+    button:hover {
+        background: #fff;
+        color: #722D78;
+    }
+    
+
+    @media screen and (max-width: 800px){
+        .howItWorks {
+            width: 90%
+        }
+    }
 `
