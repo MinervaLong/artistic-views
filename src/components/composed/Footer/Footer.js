2bd237e569fab565ecab3e73e41a62f7122347fe
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-09-20 13:54:22 +0200

    Add Privacy Policy and Terms Of use modals to Footer

--- src/components/composed/Footer/Footer.js
+++ src/components/composed/Footer/Footer.js
@@ -1,12 +1,39 @@
-
-import SocialMedia from '../SocialMedia/SocialMedia'
+import { twitterIcon, instaIcon, facebIcon } from '../../../assets/icons';
 import {FooterWrapper} from './Footer.style'
+import {useState} from 'react';
+import Modal from '../../units/Modal/Modal';
+import PrivacyPolice from '../../../documents/PrivacyPolice';
+import TermsOfUse from '../../../documents/TermsOfUse';
 
-const FooterC = () => {
+const Footer = () => {
+    const [isPrivateShowing, setIsPrivateShowing] = useState(false);
+    const privateToggle = () => setIsPrivateShowing(!isPrivateShowing);
+
+    const [isTermsShowing, setIsTermsShowing] = useState(false);
+    const termsToggle = () => setIsTermsShowing(!isTermsShowing);
     return(
-        <FooterWrapper>      
-            <SocialMedia />
+        <FooterWrapper>
+            <div className='topFooter'>                
+                <button onClick={privateToggle}>Privacy Policy</button>
+                    <Modal isShowing={isPrivateShowing} hide={privateToggle}>
+                        <h2>Privacy Police</h2>
+                        <PrivacyPolice />                    
+                    </Modal>               
+
+                <div className='socialMedia'>
+                <a target='_blank' rel='noreferrer' href='https://twitter.com' ><img alt='Twitter' src={twitterIcon}/></a>
+                <a target='_blank' rel='noreferrer' href='https://www.instagram.com'><img alt='Instagram' src={instaIcon}/></a>
+                <a target='_blank' rel='noreferrer' href='https://www.facebook.com/'><img alt='Facebook' src={facebIcon}/></a>
+                </div>
+
+                <button onClick={termsToggle}>Terms of Use</button>
+                <Modal isShowing={isTermsShowing} hide={termsToggle}>
+                    <h2>Terms of Use</h2>
+                    <TermsOfUse />                
+                </Modal>             
+            </div>
+            <div className='bottomFooter'><p>©️Artistic Views 2021</p></div> 
         </FooterWrapper>         
     )
 }
-export default FooterC;
+export default Footer;
--- src/components/composed/Footer/Footer.style.js
+++ src/components/composed/Footer/Footer.style.js
@@ -2,4 +2,44 @@ import styled from "styled-components";
 //import Colors from "theme/Colors";
 
 export const FooterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-content: space-around;
+    align-items: stretch;
+    background-color: #E1E6E2;
+    font-size: small;
+    color: #722D78;
+    font-weight: bold;
+    box-shadow:  0px -4px 5px 0px rgba(50, 50, 50, 0.29);
+
+    .topFooter {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        align-content: space-around;
+        align-items: center;
+     
+    }
+    .bottomFooter {
+        text-align: center;
+    }
+
+    .socialMedia img {
+        width: 30px;
+        height: 30px;
+        color: #722D78;
+        margin: 0.5rem 1rem 0 1rem;
+    }
+
+    button {
+        border: none;
+        background-color: #E1E6E2;
+        color: #722D78;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
 `
