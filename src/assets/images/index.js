8ba3db4c78b66b2317212e0be3f2ac8a79993977
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-09-20 13:52:31 +0200

    Add navBarLog image to index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,4 +3,4 @@ export {default as SignInImg} from './signInImg.svg';
 export {default as CoverImg} from './cover.svg';
 export {default as AboutImg} from './aboutImg.png';
 export {default as ImageHIW} from './imageHIW.png';
-export {default as NavBarLogo} from './navBarLogo.png'
+export {default as NavBarLogo} from './navBarLogo.png';
